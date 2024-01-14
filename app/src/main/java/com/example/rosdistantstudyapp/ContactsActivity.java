8845d1ecce8772b4bf73567a2db541412a3fc3dd
Kaesoron <postal0055@gmail.com>
2024-01-14 11:24:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rosdistantstudyapp/ContactsActivity.java
@@ -0,0 +1,51 @@
+package com.example.rosdistantstudyapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class ContactsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.about_me) {// Действие для пункта "Обо мне"
+            Intent intent = new Intent(this, AboutMeActivity.class);
+            startActivity(intent);
+        } else if (itemId == R.id.my_education) {// Действие для пункта "Моя учеба"
+            Intent intent = new Intent(this, MyEducationActivity.class);
+            startActivity(intent);
+        } else if (itemId == R.id.my_work) {// Действие для пункта "Моя работа"
+            Intent intent = new Intent(this, MyWorkActivity.class);
+            startActivity(intent);
+        } else if (itemId == R.id.my_hobby) {// Действие для пункта "Мое хобби"
+            Intent intent = new Intent(this, MyHobbyActivity.class);
+            startActivity(intent);
+        } else if (itemId == R.id.my_plans) {// Действие для пункта "Мои планы"
+            Intent intent = new Intent(this, MyPlansActivity.class);
+            startActivity(intent);
+        } else if (itemId == R.id.contacts) {// Действие для пункта "Контакты"
+            Intent intent = new Intent(this, ContactsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/rosdistantstudyapp/MyEducationActivity.java
@@ -0,0 +1,6 @@
+package com.example.rosdistantstudyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyEducationActivity extends AppCompatActivity {
+}
